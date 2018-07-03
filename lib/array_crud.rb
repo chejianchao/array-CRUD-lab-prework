6241567142570048cb6c96fbcae76377aa69d6a0
chejianchao <820992376@qq.com>
2018-07-03 05:56:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,8 @@ def create_an_empty_array
   return []
 end
 
-def create_an_array(4)
-  return Array.new(4)
+def create_an_array(size)
+  return Array.new(size)
 end
 
 def add_element_to_end_of_array(array, element)
